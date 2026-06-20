2989c2c79b7ebb4c45525b6fc3b30270deebe4d9
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-06-20 09:40:48 +0530

    added few changes to day3

---
+++ day3/roughwork.py
@@ -0,0 +1,37 @@
+# count = 0
+# while count < 5:
+#     print(count)
+#     count = count + 1
+
+# for i in range(0,100,2):
+#     print(i + 1)
+    
+
+# adding = 0
+# for number in range(1,101):
+#     adding += number
+# print(adding)
+
+# char = ['5', '5', '6', '7', '8']
+# text = ""
+# for i in char:
+#     text += i
+# print(text)
+
+# char = "55678"
+# text = []
+# for i in char:
+#     text.append(i)
+# print(text)
+
+# alphabet = ""
+# book = ""
+# paper =[]
+# for j in range(0,5):
+#     book += alphabet
+#     paper.append(alphabet)
+# print(paper)
+
+list = ['5', '5', '6', '7', '8']
+list.index[6]
+print(list)
