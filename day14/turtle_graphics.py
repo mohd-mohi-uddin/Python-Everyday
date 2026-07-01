dfeab247fbe7e20c79cf1cba495ee3b9a26a3fe7
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-07-01 23:11:09 +0530

    functions of turtle package

---
+++ day14/turtle_graphics.py
@@ -0,0 +1,61 @@
+import turtle
+from turtle import Turtle
+from turtle import Screen
+import random
+
+turtle.colormode(255)
+
+timmy = Turtle()
+timmy.shape("turtle")
+
+
+"""making a random path"""
+# angles = [0,90,180,270]
+
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0,255)
+#     b = random.randint(0,255)
+#     return r,g,b
+
+# move = True
+# while move:
+#     timmy.pensize(10)
+#     timmy.speed(15)
+#     timmy.color(random_color())
+#     direction = random.choice(angles)
+#     timmy.setheading(direction)
+#     timmy.forward(30)
+
+
+"""drwaing a spirograph"""
+for _ in range(75):
+    timmy.speed(0)
+    timmy.color("green")
+    timmy.circle(100)
+    timmy.right(5)
+
+"""making traingle to hexagon with different colors"""
+# def draw_Shape(number_of_sides):
+#     angle = 360/number_of_sides
+#     timmy.color("green")
+#     for j in range(number_of_sides):
+#         timmy.forward(100)
+#         timmy.right(angle)
+
+# for i in range(3,11):
+#     draw_Shape(i)
+
+"""making doted path"""
+# for i in range(10):
+#     timmy.pendown()
+#     timmy.forward(10)
+
+#     timmy.penup()
+#     timmy.forward(10)
+
+
+screen = Screen()
+screen.exitonclick()
+
+    
