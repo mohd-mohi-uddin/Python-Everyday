8c6ffc6e6408bf1197fab3555694dd1f64ae3671
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-07-03 09:23:28 +0530

    made a drawing tool using turtle package

---
+++ day15/drawing_tool.py
@@ -0,0 +1,31 @@
+import turtle
+
+timmy = turtle.Turtle()
+
+timmy.speed(0)
+
+def move_forward():
+    timmy.forward(10)
+
+def move_backward():
+    timmy.backward(10)
+
+def move_clockwise():
+    timmy.right(10)
+
+def move_anticlockwise():
+    timmy.left(10)
+
+def clear():
+    timmy.reset()
+   
+screen = turtle.Screen()
+screen.listen()
+
+screen.onkey(move_forward,"w")
+screen.onkey(move_backward,"s")
+screen.onkey(move_clockwise,"d")
+screen.onkey(move_anticlockwise,"a")
+screen.onkey(clear,"c")
+
+screen.exitonclick()
