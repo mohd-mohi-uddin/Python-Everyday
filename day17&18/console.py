16fb04c7900e38f3d102bcb5eb14260fccc4a18f
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-07-07 23:46:53 +0530

    updated paddles in pong game

--- day17&18/console.py
+++ day17&18/console.py
@@ -1,25 +1,27 @@
 from turtle import Turtle
 
 starting_positions = [0,-20,-40]
-class Console(Turtle):
+class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self,paddle_pos):
         super().__init__()
-        self.segments = []
-        
-    def make_console(self,x_cor):
-        for i in starting_positions:
-            segment = Turtle("square")
-            segment.penup()
-            segment.goto(x_cor,i)
-            self.segments.append(segment)
-            
+        self.shape("square")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.color("olive drab")
+        self.paddle_pos(paddle_pos)
+
+    def paddle_pos(self,paddle_side):
+        self.penup()
+        self.goto(paddle_side,0)
+
     def up(self):
         if self.ycor() < 300:
-            self.forward(30)
+            new_cor = self.ycor() + 50
+            self.goto(self.xcor(),new_cor)
 
     def down(self):
         if self.ycor() > -300:
-            self.back(30)
+            new_cor = self.ycor() - 50
+            self.goto(self.xcor(),new_cor)
 
 
