82e25efc04f999e7110e7aa7b98e61afdfe19c66
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-07-07 23:45:54 +0530

    added ball class to pong game

---
+++ day17&18/ball.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.shape("circle")
+        self.color("olive drab")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.increase_speed = 0.05
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x,new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.increase_speed *= 0.9
+
+    def refresh(self):
+        self.goto(0,0)
+        self.increase_speed = 0.05
+        self.x_move *= -1
+            
