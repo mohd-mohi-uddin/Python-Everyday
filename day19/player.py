3f0713d23626927b6a35c0bc39369fab448e6459
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-07-08 22:58:07 +0530

    added frog to the frogger game

---
+++ day19/player.py
@@ -0,0 +1,24 @@
+import turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+class Player(turtle.Turtle):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+        if self.ycor() < 300:
+            self.forward(MOVE_DISTANCE)
+
+    def refresh(self):
+        self.goto(STARTING_POSITION)
+    
+    
