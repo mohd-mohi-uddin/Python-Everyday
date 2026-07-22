a1ed3d06eab9eef62e70aa6ed4bd036041c2c564
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-07-22 23:51:25 +0530

    updated nato alphabet conversion project - day23

--- day23/NATO-alphabet-start/main.py
+++ day23/NATO-alphabet-start/main.py
@@ -6,7 +6,14 @@ BASE_DIR = Path(__file__).parent
 data = pandas.read_csv(BASE_DIR /"nato_phonetic_alphabet.csv")
 new_dict = {row.letter:row.code for index,row in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter your name:").upper()
-output_list = [new_dict[i] for i in user_input]
-print(output_list)
+def generate_letters():
+    user_input = input("Enter your name:").upper()
+    try:
+        output_list = [new_dict[i] for i in user_input]
+    except KeyError:
+        print("please enter only text")
+        generate_letters()
+    else:
+        print(output_list)
+
+generate_letters()
