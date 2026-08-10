d525c0a3728b12f00326fe2117327fbd6acac434
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-08-10 23:45:12 +0530

    added bs4 package to day40 of my python code

---
+++ day40/bs4-start/main.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+# from pathlib import Path
+
+# BASE_DIR = Path(__file__).parent
+
+# with open(BASE_DIR/"website.html") as file:
+#     contents = file.read()
+
+# soup = BeautifulSoup(contents,"html.parser")
+
+
+# heading = soup.find(name="h3",class_ ="heading")
+# print(f'is this the {str(heading.get("class"))[2:9]}')
+
+response = requests.get("https://news.ycombinator.com/")
+response.raise_for_status()
+hn_web_page = response.text
+
+soup = BeautifulSoup(hn_web_page,"html.parser")
+
+article_tag = soup.select(".athing .title .titleline > a")
+article_title = [title.getText() for title in article_tag]
+article_link = [title.get("href") for title in article_tag]
+article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name ="span",class_ = "score")]
+# print(article_title)
+# print(article_link)
+# print(article_upvote)
+
+maximum_votes = max(article_upvote)
+index_of_max_vote = article_upvote.index(maximum_votes)
+
+title_with_max_votes = article_title[index_of_max_vote]
+print(title_with_max_votes)
---
+++ day40/bs4-start/notes.md
@@ -0,0 +1,34 @@
+1. TO USE BEAUTIFUL SOAP:
+
+use classname(contents u copied using file.read(), the parser-html.parser)
+
+2. prettify()
+
+we can use ,prettify tothe obect to make the structure good.
+
+3. object.find_all(name="")
+
+this is used to find all the tags with the name: h1 or a or p in the list
+
+4. tag.getText():
+
+to get the text of that tag
+
+5. object.find(name="a", id="" or class_ = ""or any other attribute)
+
+we can get a particular tag
+
+6. tag.get("href" or any attribute):
+
+to get the particular attriobute value.
+
+7. tag.select_one(selector = "tag,class,id")
+
+to select one from the body
+
+8. tag.select(selector = "tag,class,id")
+ to get all from the body
+
+9. css selector object.select(parent tag then child tag then its child tag)
+
+
