6cf72975344b98be6edf188ef8f8ae7110e2ea85
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-08-16 23:40:31 +0530

    added interaction.py to interact with website using selenium web driver

--- day43/Selenium web driver/interaction.py
+++ day43/Selenium web driver/interaction.py
@@ -1,14 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
 
-driver = webdriver.Chrome()
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://appbrewery.github.io/fake-newsletter-signup/")
 
-lines = driver.find_element(By.XPATH, value= '//*[@id="mwDw"]')
-print(lines.text)
+time.sleep(2)
+
+first_name = driver.find_element(By.NAME, value= "fName")
+first_name.send_keys("mohi")
+
+last_name = driver.find_element(By.NAME, value= "lName")
+last_name.send_keys("don")
+
+email = driver.find_element(By.NAME, value= "email")
+email.send_keys("mohidon@gmail.cum")
+
+signup = driver.find_element(By.XPATH, value= '//*[@id="signup-form"]/button')
+signup.click()
 
 
-driver.close()
+
