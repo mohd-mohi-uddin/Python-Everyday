8038e69017b2d9f9930645208db2e0daf0fa3078
mohd-mohi-uddin <mohdmohiuddin1409@gmail.com>
2026-09-03 23:49:25 +0530

    added html code to our flask minimal app

--- d-50/FLask project/main.py
+++ d-50/FLask project/main.py
@@ -4,11 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def greet():
-    return "hello world"
+    return '<h1>good morning<h1>' \
+    '<a href = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZNP055bl-sNYr3eznKkyZFb_UwD2I7JYn7Q77J-DuuQ&s=10">wake up is morning</a>'\
+    '<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuNmuhVyOgKHFi7QLM3jKRktTW9RF6LE3BdsIN6mqxMg&s=10">'
 
-@app.route("/bye/<name>")
+@app.route("/<name>")
 def good_bye(name):
     return f"bye {name}"
 
 app.run()
+
+
+
+
+
 
